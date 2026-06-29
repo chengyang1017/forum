915abc842345cb1285d26d124943122b10c146f5
chengyang1017 <chengyang1017@gmail.com>
2026-06-29 10:03:28 +0800

    Update home_tab.dart

--- lib/screens/home_tab.dart
+++ lib/screens/home_tab.dart
@@ -20,6 +20,7 @@ class HomeTab extends StatelessWidget {
     {"name": "闲聊", "icon": Icons.chat},
     {"name": "爱情", "icon": Icons.favorite_border},
     {"name": "美食", "icon": Icons.restaurant},
+    {"name": "合作", "icon": Icons.collaboration},
   ];
 
   @override
