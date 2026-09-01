5a80625adbf00203dd2bc98b96a20614c1b45b73
Lim Cheng Yang <chengyang1017@gmail.com>
2026-09-02 04:36:48 +0800

    refactor(flutter): add AppLanguage state

---
+++ apps/mobile-flutter/lib/app/cubit/app_language_state.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AppLanguageState {
+  const AppLanguageState({
+    this.locale = const Locale('zh'),
+  });
+
+  final Locale locale;
+
+  AppLanguageState copyWith({
+    Locale? locale,
+  }) {
+    return AppLanguageState(
+      locale: locale ?? this.locale,
+    );
+  }
+}
