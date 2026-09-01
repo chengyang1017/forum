2b86c22f34829eaac837dbb5fb737edb7563001d
Lim Cheng Yang <chengyang1017@gmail.com>
2026-09-02 03:58:03 +0800

    refactor(flutter): add feed state

---
+++ apps/mobile-flutter/lib/features/feed/presentation/cubit/feed_state.dart
@@ -0,0 +1,20 @@
+class FeedState {
+  const FeedState({
+    this.isLoading = false,
+    this.error,
+  });
+
+  final bool isLoading;
+  final String? error;
+
+  FeedState copyWith({
+    bool? isLoading,
+    String? error,
+    bool clearError = false,
+  }) {
+    return FeedState(
+      isLoading: isLoading ?? this.isLoading,
+      error: clearError ? null : error ?? this.error,
+    );
+  }
+}
