75b6ba9f0ccdd704fcf769ec3ade2595b0b4695a
Lim Cheng Yang <chengyang1017@gmail.com>
2026-09-02 04:50:04 +0800

    refactor(auth): use flutter_bloc in forgot password screen

--- apps/mobile-flutter/lib/features/auth/presentation/screens/forgot_password_screen.dart
+++ apps/mobile-flutter/lib/features/auth/presentation/screens/forgot_password_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
-import 'package:provider/provider.dart';
 
 import '../../../../app/router/app_routes.dart';
 import '../cubit/auth_cubit.dart' as auth_cubit;
