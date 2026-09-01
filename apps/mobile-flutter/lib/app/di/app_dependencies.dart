602fde837901d63c5d7f2dbbea6a0759813ee7d2
chengyang1017 <chengyang1017@gmail.com>
2026-09-01 17:41:10 +0800

    refactor(flutter): compose profile dependencies

--- apps/mobile-flutter/lib/app/di/app_dependencies.dart
+++ apps/mobile-flutter/lib/app/di/app_dependencies.dart
@@ -2,6 +2,10 @@ import '../../features/post/application/ports/post_media_repository.dart';
 import '../../features/post/data/repositories/firebase_post_media_repository.dart';
 import '../../features/post/data/repositories/post_repository_impl.dart';
 import '../../features/post/domain/repositories/post_repository.dart';
+import '../../features/profile/application/ports/profile_media_repository.dart';
+import '../../features/profile/data/repositories/firebase_profile_media_repository.dart';
+import '../../features/profile/data/repositories/profile_repository_impl.dart';
+import '../../features/profile/domain/repositories/profile_repository.dart';
 
 /// Application-level dependency container.
 ///
@@ -12,15 +16,21 @@ final class AppDependencies {
   AppDependencies({
     required this.postRepository,
     required this.postMediaRepository,
+    required this.profileRepository,
+    required this.profileMediaRepository,
   });
 
   factory AppDependencies.create() {
     return AppDependencies(
       postRepository: PostRepositoryImpl(),
       postMediaRepository: FirebasePostMediaRepository(),
+      profileRepository: ProfileRepositoryImpl(),
+      profileMediaRepository: FirebaseProfileMediaRepository(),
     );
   }
 
   final PostRepository postRepository;
   final PostMediaRepository postMediaRepository;
+  final ProfileRepository profileRepository;
+  final ProfileMediaRepository profileMediaRepository;
 }
